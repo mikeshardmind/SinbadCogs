2887526b93c0bb7ca460a33b2ea02034d4b163c5
michael <michael@michaelhall.tech>
2017-03-20 17:37:14 -0400

    Everything is working now, version1.0

--- announcer/announcer.py
+++ announcer/announcer.py
@@ -14,7 +14,7 @@ class Announcer:
     """Configureable Announcements."""
 
     __author__ = "mikeshardmind"
-    __version__ = "0.1"
+    __version__ = "1.0"
 
     def __init__(self, bot):
         self.bot = bot
@@ -34,41 +34,43 @@ class Announcer:
             if server_id in self.settings:
                 server = self.bot.get_server(server_id)
                 channel = server.get_channel(self.settings[server_id]['channel'])
-                if channel.permissions_for(me).send_messages:
+                if channel.permissions_for(server.me).send_messages:
                     await self.bot.send_message(channel, msg)
 
     @checks.is_owner()
     @commands.group(name="announcerset", pass_context=True)
-    async def announcerset(self, ctx, *, msg):
+    async def announcerset(self, ctx):
         """Settings for announcer"""
 
         if ctx.invoked_subcommand is None:
             await self.bot.send_cmd_help(ctx)
 
-    @check.is_owner()
+    @checks.is_owner()
     @announcerset.command(name="addchan", pass_context=True)
-    async def addchan(self, ctx, *, channel_id):
+    async def addchan(self, ctx, *, channel_id=None):
         """adds a channel to the announcer's channel list
         defaults to the current channel, can optionally be given
         a server id & channel id
         Will not announce to Direct Message"""
 
         channel = None
+        server = None
         if channel_id is None:
             channel = ctx.message.channel
             server = ctx.message.server
         else:
-            for serv in client.servers:
-                if serv.get_channel(channel_id) is not None:
-                    channel = serv.get_channel(channel_id)
-                    server = serv
+            for serv in self.bot.servers:
+                for chan in serv.channels:
+                    if chan.id == channel_id:
+                        channel = chan
+                        server = serv
 
         if channel is None:
             await self.bot.say("I cannot find that channel")
         elif channel.is_private:
             await self.bot.say("Ignoring Request: Invalid place to send announcements")
         else:
-            if channel.permissions_for(me).send_message is False:
+            if channel.permissions_for(server.me).send_messages is False:
                 await self.bot.say("Warning: I cannot speak in that channel "
                                    "I will add it to the list, but announcements"
                                    " will not be sent if this is not fixed")
@@ -81,31 +83,33 @@ class Announcer:
             await self.bot.say("Announcement channel for the associated server "
                                "has been set")
 
-    @check.is_owner()
+    @checks.is_owner()
     @announcerset.command(name="delchan", pass_context=True)
-    async def delchan(self, ctx, *, channel_id):
+    async def delchan(self, ctx, *, channel_id=None):
         """removes a channel from the announcements list
         defaults to current if not given a channel id"""
 
         channel = None
+        server = None
         if channel_id is None:
             channel = ctx.message.channel
             server = ctx.message.server
         else:
-            for serv in client.servers:
-                if serv.get_channel(channel_id) is not None:
-                    channel = serv.get_channel(channel_id)
-                    server = serv
+            for serv in self.bot.servers:
+                for chan in serv.channels:
+                    if chan.id == channel_id:
+                        channel = chan
+                        server = serv
 
         if server.id in self.settings:
-            if channel.id == self.settings[server.id]:
+            if channel.id == self.settings[server.id]['channel']:
                 self.settings[server.id]['channel'] = None
                 await self.bot.say("Channel removed from announcment list")
                 self.save_settings()
             else:
                 await self.bot.say("This is not an announcement channel")
                 await self.bot.say("Hint: The announcement channel"
-                                   "for the server is <#{}>".format(channel.id))
+                                   " for the server is <#{}>".format(self.settings[server.id]['channel']))
         else:
             await self.bot.say("This channel is not an announcement channel")
 
