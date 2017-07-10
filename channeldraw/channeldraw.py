213aa48572c57c08c91d49710273d02d69060bcb
michael <michael@michaelhall.tech>
2017-07-10 02:18:50 -0400

    Untested, don't push yet

--- channeldraw/channeldraw.py
+++ channeldraw/channeldraw.py
@@ -9,53 +9,47 @@ import asyncio
 
 
 class ChannelDraw:
+    """Draws a random message from a set"""
 
     __author__ = "mikeshardmind"
-    __version__ = "1.1"
+    __version__ = "2.0"
 
     def __init__(self, bot):
         self.bot = bot
-        self.queue = []
-        self.user = None
-        self.locked = False
+        self.users = []
+        self.locks = []
         self.settings = dataIO.load_json('data/channeldraw/settings.json')
 
     def save_json(self):
         dataIO.save_json("data/channeldraw/settings.json", self.settings)
 
-    @checks.is_owner()
+    @checks.admin_or_permissions(Manage_channels=True)
     @commands.group(pass_context=True, name='draw', no_pm=True)
     async def draw(self, ctx):
-        """Used for the weekly portal draw"""
+        """Need I say more?"""
         if ctx.invoked_subcommand is None:
             await self.bot.send_cmd_help(ctx)
 
-    @draw.command(pass_context=True, name='bymessages')
+    @draw.command(pass_context=True, name='bymessageids', aliases=["bmids"])
     async def by_msgs(self, ctx, first: str, last: str):
-        """gets messages of an inclusive range of the message IDs
-        in the same channel"""
 
+        if ctx.message.author.id in self.users:
+            return await self.bot.say("You already have a drawing in progress")
         a = await self.get_msg(first)
         b = await self.get_msg(last)
         if a is None or b is None:
             return await self.bot.say("I could not find one or both of those")
         if a.channel.id != b.channel.id:
             return await self.bot.say("Those messages are in seperate rooms")
-        if self.locked:
-            return await self.bot.say("<@{}> has already started the drawing "
-                                      .format(self.user.id))
+        if a.channel.id in self.locks:
+            return await self.bot.say("That channel has a drawing in progress")
         if a.timestamp >= b.timestamp:
-            return await self.bot.send_cmd_help(ctx)
+            a, b = b, a  # Because I can't trust people to use things correctly
 
-        self.locked = True
-        self.user = ctx.message.author
-        self.queue.append(a)
-        await self.mkqueue(a.timestamp, b.timestamp, b.channel)
-        self.queue.append(b)
-
-        self.settings['latest'] = ctx.message.timestamp.strftime("%Y%m%d%H%M")
-        self.save_json()
-        await self.validate(b.channel)
+        msgs = await self.mkqueue(a.timestamp, b.timestamp, b.channel)
+        msgs.append(a)
+        msgs.append(b)
+        await self.validate(msgs, ctx.message.author)
 
     @draw.command(pass_context=True, name='bytimes')
     async def by_times(self, ctx, *, times):
@@ -73,21 +67,20 @@ class ChannelDraw:
         except ValueError:
             return await self.bot.send_cmd_help(ctx)
         if a >= b:
-            return await self.bot.send_cmd_help(ctx)
+            a, b = b, a
+        if b > ctx.message.timestamp:
+            b = ctx.message.timestamp
+        if a >= ctx.message.timestamp:
+            return await self.bot.say("I can't read the future.")
+        if ctx.channel.id in self.locks:
+            return await self.bot.say("That channel has a drawing in progress")
+        if ctx.message.author.id in self.users:
+            return await self.bot.say("You already have a drawing in progress")
 
-        if self.locked:
-            return await self.bot.say("<@{}> has already started the drawing "
-                                      .format(self.user.id))
+        msgs = await self.mkqueue(a, b, ctx.message.channel)
+        await self.validate(msgs, ctx.message.author)
 
-        self.locked = True
-        self.user = ctx.message.author
-
-        await self.mkqueue(a, b, ctx.message.channel)
-        self.settings['latest'] = ctx.message.timestamp.strftime("%Y%m%d%H%M")
-        self.save_json()
-        await self.validate(ctx.message.channel)
-
-    @draw.command(pass_context=True, name='bytimespan')
+    @draw.command(pass_context=True, name='fromdate')
     async def by_interval(self, ctx, *, time):
         """gets messages from the channel it was called from
         between now and a time (UTC).\n
@@ -98,55 +91,68 @@ class ChannelDraw:
             t = str(time)
             t = ''.join(c for c in t if c.isdigit())
             a = dt.strptime(t, "%Y%m%d%H%M")
-            b = dt.utcnow()
+            b = dt.utcnow
             pass
         except ValueError:
             return await self.bot.send_cmd_help(ctx)
         if a >= b:
             return await self.bot.send_cmd_help(ctx)
+        if ctx.message.author.id in self.users:
+            return await self.bot.say("You already have a drawing in progress")
+        if ctx.message.channel.id in self.locks:
+            return await self.bot.say("That channel has a drawing in progress")
 
-        if self.locked:
-            return await self.bot.say("<@{}> has already started the drawing "
-                                      .format(self.user.id))
-
-        self.locked = True
-        self.user = ctx.message.author
-        await self.mkqueue(a, b, ctx.message.channel)
-        self.settings['latest'] = ctx.message.timestamp.strftime("%Y%m%d%H%M")
-        self.save_json()
-        await self.validate(ctx.message.channel)
+        msgs = await self.mkqueue(a, b, ctx.message.channel)
+        await self.validate(msgs, ctx.message.author)
 
     @draw.command(name="auto", pass_context=True)
     async def autodraw(self, ctx):
         """only works if there is a prior draw on record"""
-        if not self.settings['latest']:
+        if not self.settings[ctx.message.channel.id]['latest']:
             return await self.bot.send_cmd_help(ctx)
+        if ctx.message.author.id in self.users:
+            return await self.bot.say("You already have a drawing in progress")
+        if ctx.message.channel.id in self.locks:
+            return await self.bot.say("That channel has a drawing in progress")
 
-        if self.locked:
-            return await self.bot.say("<@{}> has already started the drawing "
-                                      .format(self.user.id))
-
-        self.locked = True
-        self.user = ctx.message.author
-        a = dt.strptime(self.settings['latest'], "%Y%m%d%H%M")
+        a = dt.strptime(self.settings[ctx.message.channel.id]['latest'],
+                        "%Y%m%d%H%M")
         b = ctx.message.timestamp
-        await self.mkqueue(a, b, ctx.message.channel)
+        msgs = await self.mkqueue(a, b, ctx.message.channel)
         await self.validate(ctx.message.channel)
 
         self.settings['latest'] = ctx.message.timestamp.strftime("%Y%m%d%H%M")
         self.save_json()
 
-    async def validate(self, channel):
+    async def validate(self, msgs=[], author):
+        temp_latest = b.timestamp
+        fail_count = 0
         random.seed()
-        random.shuffle(self.queue)
-        while self.locked:
-            dm = await self.bot.send_message(self.user,
-                                             "Is the following a valid entry?")
+        random.shuffle(msgs)
+
+        while author.id in self.locks:
+            if fail_count > 3:
+                await self.bot.send_message(author,
+                                            "A report of your inability "
+                                            "to answer a yes or no question "
+                                            "has been generated for review")
+                await self.shame(author)
+                break
+            if len(msgs) == 0:
+                await self.bot.send_message(author, "That's all folks")
+                break
+            dm = await self.bot.send_message(author,
+                                             "Is the following a valid entry?"
+                                             "(yes/no/quit)")
             entry = self.queue.pop()
             em = self.qform(entry)
-            await self.bot.send_message(self.user, embed=em)
-            message = await self.bot.wait_for_message(channel=dm.channel,
-                                                      author=self.user)
+            await self.bot.send_message(author, embed=em)
+            message = await self.bot.wait_for_message(
+                                                channel=dm.channel,
+                                                author=author, timeout=60)
+            if message is None:
+                fail_count += 1
+                continue
             reply = message.clean_content.lower()
 
             if reply[0] == 'y':
@@ -154,13 +160,16 @@ class ChannelDraw:
                                             "the following entry"
                                             "".format(entry.author.mention))
                 await self.bot.send_message(channel, embed=em)
-                self.queue = []
-                self.locked = False
+                self.settings[a.channel.id]['latest'] = \
+                    temp_latest.strftime("%Y%m%d%H%M")
+                self.save_json()
             if reply[0] == 'n':
                 await self.bot.send_message(self.user, "Ok then...")
-                if len(self.queue) == 0:
-                    await self.bot.send_message(self.user, "That's all folks")
-                    self.locked = False
+            if reply[0] == 'q':
+                await self.bot.send_message(self.user,
+                                            "I guess we're done here")
+        self.users.remove(author)
+        self.locks.remove(channel)
 
     async def mkqueue(self, a, b, channel):
 
