70dcd2ce364c11fb1ff3142b0e79dfa99f7117ef
michael <michael@michaelhall.tech>
2017-11-12 12:29:42 -0500

    role support

--- permbreaker/permbreaker.py
+++ permbreaker/permbreaker.py
@@ -33,6 +33,41 @@ class PermBreaker:
         if ctx.invoked_subcommand is None:
             await self.bot.send_cmd_help(ctx)
 
+    @pbreak.group(name='role', pass_context=True)
+    async def pbreak_role(self, ctx):
+        """"
+        role specific settings
+        """
+        if ctx.invoked_subcommand is None:
+            await self.bot.send_cmd_help(ctx)
+
+    @pbreak_role.command(name="allow", pass_context=True)
+    async def pbreak_role_allow(self, ctx, command_name, *roles: discord.Role):
+        """
+        allow (a) specifc role(s) to use a command
+
+        multiword commands need to be enclosed in quotes
+
+        multiword roles also need to be enclosed in quotes
+        This has security implications, be careful with it
+        """
+        if len(roles) == 0:
+            return await self.bot.send_cmd_help(ctx)
+        com = self.bot.get_command(command_name)
+        if com is None:
+            return await self.bot.say("No such command")
+        if com.cog_name == 'PermBreaker':
+            return await self.bot.say(
+                "If you are going to allow this, "
+                "you may as well just make them a coowner")
+
+        current = self.settings.get(com.name, [])
+        current.extend([x.id for x in roles])
+        self.settings[com.name] = current
+        self.save_settings()
+
+        await self.bot.say('Bypass made')
+
     @pbreak.command(name='allow', pass_context=True)
     async def pbreak_allow(
             self, ctx, command_name, *who: discord.Member):
@@ -61,14 +96,37 @@ class PermBreaker:
 
         await self.bot.say('Bypass made')
 
+    @pbreak_role.command(name="disallow", pass_context=True)
+    async def pbreak_role_disallow(
+            self, ctx, command_name, *roles: discord.Role):
+        """
+        removes roles from being allowed to use a command
+
+        multiword commands need to be enclosed in quotes
+
+        the same applies to mutiword roles
+        """
+        if len(roles) == 0:
+            return await self.bot.send_cmd_help(ctx)
+        com = self.bot.get_command(command_name)
+        if com is None:
+            return await self.bot.say("No such command")
+
+        if com.name not in self.settings:
+            return await self.bot.say('No settings for this command to modify')
+
+        self.settings[com.name] = [x for x in self.settings[com.name]
+                                   if x not in [y.id for y in roles]]
+        self.save_settings()
+
+        await self.bot.say('Settings updated')
+
     @pbreak.command(name='disallow', pass_context=True)
     async def pbreak_disallow(self, ctx, command_name, *who: discord.Member):
         """
         removes people from being allowed to use a command
 
         multiword commands need to be enclosed in quotes
-
-        This has security implications, be careful with it
         """
         if len(who) == 0:
             return await self.bot.send_cmd_help(ctx)
@@ -82,6 +140,20 @@ class PermBreaker:
         self.settings[com.name] = [x for x in self.settings[com.name]
                                    if x not in [y.id for y in who]]
         self.save_settings()
+
+        await self.bot.say('Settings updated')
+
+    @pbreak_role.command(name='disallowall', pass_context=True)
+    async def pbreak_role_disallowall(
+            self, ctx, command_name, *roles: discord.Role):
+        """
+        removes any entries for a role
+        """
+        if len(roles) == 0:
+            return await self.bot.send_cmd_help(ctx)
+
+        for k, v in self.settings.items():
+            self.settings[k] = [x for x in v if x not in [y.id for y in roles]]
 
         await self.bot.say('Settings updated')
 
@@ -131,9 +203,13 @@ class PermBreaker:
 
         data = {}
         who = [x for x in self.bot.get_all_members()]
+        roles = []
+        for server in self.bot.servers:
+            roles.extend(server.roles)
         for k, v in self.settings.items():
             users = [u for u in who if u.id in v]
-            data[k] = {'u': users}
+            roles = [r for r in roles if r.id in v]
+            data[k] = {'u': users, 'r': roles}
 
         output = ""
         for k, v in data.items():
@@ -141,6 +217,10 @@ class PermBreaker:
             output += "\nUsers: "
             for user in v['u']:
                 output += "{} ".format(user.mention)
+            output += "\nRoles: "
+            for role in v['r']:
+                output += "\nRole: {0.name} | Server: {0.server.name}".format(
+                    role)
 
         if len(output) == 0:
             return await self.bot.say('No config set yet')
@@ -155,16 +235,21 @@ class PermBreaker:
             # a coowner
             return
 
-        if self.can_bypass_checks(ctx.message.author, ctx.command):
+        if self.can_bypass_checks(ctx):
             await self.bypass_checks(ctx)
 
-    def can_bypass_checks(self, who, com):
+    def can_bypass_checks(self, ctx):
 
-        if com.name not in self.settings:
+        if ctx.command.name not in self.settings:
             return False
-
-        flakes = self.settings[com.name]
-        return who.id in flakes
+        flakes = self.settings[ctx.command.name]
+        if ctx.message.author.id in flakes:
+            return True
+        elif ctx.message.server:
+            role_flakes = [r.id for r in ctx.message.author.roles]
+            return not set(role_flakes).isdisjoint(flakes)
+        else:
+            return False
 
     async def bypass_checks(self, ctx):
         # please don't kill me Danny
