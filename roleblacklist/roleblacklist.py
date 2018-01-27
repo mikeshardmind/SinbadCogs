a8cd17104c39dba0e85811301d8c2eaa60cfa4d4
michael <michael@michaelhall.tech>
2018-01-26 21:29:33 -0500

    role bl

--- roleblacklist/roleblacklist.py
+++ roleblacklist/roleblacklist.py
@@ -5,12 +5,12 @@ from cogs.utils.dataIO import dataIO
 from cogs.utils import checks
 from __main__ import settings
 
-path = 'data/rolechecker'
+path = 'data/roleblacklist'
 
 
 class RoleChecker:
     """
-    Require a role to use the bot
+    Role based blacklist
     """
 
     __version__ = "1.0.0"
@@ -24,37 +24,42 @@ class RoleChecker:
         try:
             self.roles = dataIO.load_json(path + '/settings.json')
         except Exception:
-            self.roles = []
+            self.roles = {}
 
     def save_roles(self):
         dataIO.save_json(path + '/settings.json', self.roles)
 
-    @commands.command(pass_context=True, name='setreqroles')
-    @checks.is_owner()
+    @commands.command(pass_context=True, name='roleblacklist', no_pm=True)
+    @checks.admin_or_permissions(manage_server=True)
     async def set_required_role(self, ctx, *roles: discord.Role):
         """
-        sets the role(s) required to use the bot
+        sets the role(s) which cannot use the bot
         multi word roles should be surrounded with quotes
 
-        WARNING: make sure people cannot assign themselves any of these roles
+        WARNING: make sure people cannot remove any of these roles
         with another bot or something
-
-        To disable this check, unload the cog
         """
+        server = ctx.message.server
         if len(roles) > 0:
-            self.roles = [r.id for r in roles]
+            self.roles[server.id] = [r.id for r in roles]
             self.save_roles()
-            await self.bot.say('Role set')
+            await self.bot.say('Role(s) set')
         else:
             await self.bot.send_cmd_help(ctx)
 
     def __check(self, ctx):
         allowed = False
         author = ctx.message.author
+        try:
+            server = author.server
+        except AttributeError:
+            return True
         allowed |= author.id == settings.owner
         if isinstance(author, discord.Member):
-            allowed |= len([r for r in author.roles
-                            if r.id in self.roles]) > 0
+            allowed |= len(
+                [r for r in author.roles
+                 if r.id in self.roles[server.id]]
+            ) == 0
         return allowed
 
 
