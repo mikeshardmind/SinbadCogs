d58f9abc3cad1d186519a1203ff49ebaa5024ee9
michael <michael@michaelhall.tech>
2018-02-21 19:41:00 -0500

    version bump

--- multiwayrelay/multiwayrelay.py
+++ multiwayrelay/multiwayrelay.py
@@ -15,7 +15,7 @@ class MultiWayRelay:
     """
 
     __author__ = "mikeshardmind (Sinbad#0001)"
-    __version__ = "2.0.3"
+    __version__ = "2.1.0"
 
     def __init__(self, bot):
         self.bot = bot
