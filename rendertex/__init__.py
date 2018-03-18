6e1ab593dca1227a5795fbe20a3a30d70be520af
Michael H <Sinbad@michaelhall.tech>
2018-03-18 00:30:35 -0400

    Update __init__.py

--- rendertex/__init__.py
+++ rendertex/__init__.py
@@ -3,7 +3,4 @@ import sys
 
 
 def setup(bot):
-    if sys.version_info < (3, 6, 0):
-        raise RuntimeError("Requires python >= 3.6")
-    else:
-        bot.add_cog(RenderTex(bot))
+    raise NotImplementedError(".")
