bc813073cecb86bd28724e6b54227d86e1b5e327
michael <michael@michaelhall.tech>
2018-04-23 21:43:49 -0400

    removing this until I fix it

--- calculator/__init__.py
+++
@@ -1,9 +0,0 @@
-from .calc import Calculator
-import sys
-
-
-def setup(bot):
-    if sys.platform != 'linux':
-        raise RuntimeWarning("This doesn't work on your OS")
-    else:
-        bot.add_cog(Calculator(bot))
