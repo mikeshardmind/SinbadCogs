d4b0e5c0e4756f889332addccfca046b0330a6d9
michael <michael@michaelhall.tech>
2018-04-28 15:18:12 -0400

    hopefully fix this

--- embedmaker/serialize.py
+++ embedmaker/serialize.py
@@ -1,60 +1,45 @@
 import discord
 from datetime import datetime as dt
-from copy import copy
-
-template = {
-    'initable': {
-        'description': None,
-        'color': None,
-        'title': None,
-        'url': None,
-        'timestamp': None
-    },
-    'settable': {
-        'image': {'url': None},
-        'thumbnail': {'url': None},
-        'author': {'url': None, 'name': None, 'icon_url': None},
-        'footer': {'text': None, 'icon_url': None}
-    },
-    'multiadded': {
-        'fields': [],
-    }
-}
 
 
 def serialize_embed(embed: discord.Embed) -> dict:
-
-    ret = {
-        'initable': {},
-        'settable': {},
-        'multiadded': {}
-    }
     data = embed.to_dict()
-
-    for k, v in template.items():
-        for attr in v.keys():
-            if attr in data:
-                ret[k][v][attr] = embed.timestamp.timestamp(
-                ) if attr == 'timestamp' else data[attr]
-
-    return ret
+    if embed.timestamp:
+        data['timestamp'] = embed.timestamp.timestamp()
 
 
 def deserialize_embed(conf: dict) -> discord.Embed:
 
-    unpack = copy(conf['initable'])
+    unpack = {
+        k: v for k, v in conf
+        if k in (
+            'description',
+            'color',
+            'title',
+            'url',
+            'timestamp'
+        )
+    }
 
-    if 'timestamp' in unpack:
+    settable = {
+        k: v for k, v in conf
+        if k in (
+            'image',
+            'thumbnail',
+            'author',
+            'footer'
+        )
+    }
+
+    if unpack.get('timestamp', None):
         unpack['timestamp'] = dt.utcfromtimestamp(unpack['timestamp'])
 
     e = discord.Embed(**unpack)
 
-    for k, v in conf['settable'].items():
+    for k, v in {settable.items()}:
         getattr(e, 'set_' + k)(**v)
 
-    # right now this is just fields, but why not just in case?
-    for k, v in conf['multiadded'].items():
-        for entry in v:
-            getattr(e, 'add_{}'.format(k[:-1]))(**entry)
+    for field in conf.get('fields', []):
+        e.add_field(**field)
 
     return e
