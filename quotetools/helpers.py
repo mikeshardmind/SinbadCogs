49871cd529b3eb165d7ea275f2d660cb57dd1640
michael <michael@michaelhall.tech>
2018-05-12 18:48:38 -0400

    WIP, probably works as-is

---
+++ quotetools/helpers.py
@@ -0,0 +1,92 @@
+import discord
+import re
+from discord.ext import commands
+
+
+def role_mention_cleanup(message: discord.Message) -> str:
+
+    if message.guild is None:
+        return message.content
+
+    transformations = {
+        re.escape('<@&{0.id}>'.format(role)): '@' + role.name
+        for role in message.role_mentions
+    }
+
+    def repl(obj):
+        return transformations.get(re.escape(obj.group(0)), '')
+
+    pattern = re.compile('|'.join(transformations.keys()))
+    result = pattern.sub(repl, message.content)
+
+    return result
+
+
+def embed_from_msg(message: discord.Message) -> discord.Embed:
+    channel = message.channel
+    server = channel.guild
+    content = role_mention_cleanup(message)
+    author = message.author
+    sname = server.name
+    cname = channel.name
+    avatar = author.avatar_url
+    footer = 'Said in {} #{}'.format(sname, cname)
+    em = discord.Embed(description=content, color=author.color,
+                       timestamp=message.created_at)
+    em.set_author(name='{}'.format(author.name), icon_url=avatar)
+    em.set_footer(text=footer, icon_url=server.icon_url)
+    if message.attachments:
+        a = message.attachments[0]
+        fname = a.filename
+        url = a.url
+        if fname.split('.')[-1] in ['png', 'jpg', 'gif', 'jpeg']:
+            em.set_image(url=url)
+        else:
+            em.add_field(name='Message has an attachment',
+                         value='[{}]({})'.format(fname, url),
+                         inline=True)
+
+    return em
+
+
+async def eligible_channels(ctx: commands.Context) -> list:
+    """
+    Get's the eligible channels to check
+    """
+
+    ret = []
+    is_owner = await ctx.bot.is_owner(ctx.author)
+    needed_perms = discord.Permissions()
+    needed_perms.read_messages = True
+    needed_perms.read_message_history = True
+    guild_order = [g for g in ctx.bot.guilds if g != ctx.guild]
+    if ctx.guild:
+        guild_order.insert(0, ctx.guild)
+
+    for g in ctx.bot.guilds:
+        chans = [
+            c for c in g.text_channels
+            if c.permissions_for(g.me) >= needed_perms
+            and (
+                is_owner
+                or c.permissions_for(ctx.author) >= needed_perms
+            )
+        ]
+        if ctx.channel in chans:
+            chans.remove(ctx.channel)
+            chans.insert(0, ctx.channel)
+
+        ret.extend(chans)
+
+    return ret
+
+
+async def find_msg(ctx: commands.Context, idx: int) -> discord.Message:
+
+    async for channel in eligible_channels(ctx):
+        try:
+            m = await channel.get_message(idx)
+        except Exception:
+            continue
+        else:
+            return m
---
+++ quotetools/quotetools.py
@@ -0,0 +1,41 @@
+from discord.ext import commands
+
+from .helpers import find_msg, embed_from_msg
+
+
+class QuoteTools:
+    """
+    Cog for quoting messages by ID
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def quote(self, ctx, messageid):
+        """
+        gets a message by ID
+
+        User must be able to see the message
+        """
+
+        m = await find_msg(messageid)
+        if m is None:
+            return await ctx.maybe_send_embed("No matching message found.")
+
+        if await ctx.embed_requested():
+            em = embed_from_msg(m)
+            await ctx.send(embed=em)
+        else:
+            msg1 = "\n".join([
+                "Author: {0}({0.id})".format(m.author),
+                "Channel: {}".format(m.channel.mention),
+                "Time(UTC): {}".format(m.timestamp.isoformat())
+            ])
+            if len(msg1) + len(m.clean_content) < 2000:
+                await ctx.send(
+                    msg1 + m.clean_content
+                )
+            else:
+                await ctx.send(msg1)
+                await ctx.send(m.clean_content)
