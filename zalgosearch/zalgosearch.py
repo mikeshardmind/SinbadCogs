52f20078082c2c0b6f52969834e4f46de287ba02
Michael H <michael@michaelhall.tech>
2018-07-24 02:58:13 -0400

    version bump + extra safety

--- zalgosearch/zalgosearch.py
+++ zalgosearch/zalgosearch.py
@@ -16,10 +16,12 @@ class ZalgoSearch:
 
     """
     Replaces display names using zalgo chars.
+
+    This only bothers replacing zalgo names which overflow vertically.
     """
 
     __author__ = "mikeshardmind"
-    __version__ = "1.0.0b"
+    __version__ = "1.0.1b"
 
     def __init__(self, bot: "Red"):
         self.bot = bot
@@ -32,6 +34,7 @@ class ZalgoSearch:
         for member in members:
             if ZALGO_REGEX.match(member.display_name):
                 yield member
+            await asyncio.sleep(0.1)
 
     async def on_member_join(self, member: discord.Member):
         if member.guild.me.guild_permissions.manage_nicknames:
@@ -46,14 +49,25 @@ class ZalgoSearch:
     @commands.guild_only()
     @checks.mod_or_permissions(manage_nicknames=True)
     @commands.bot_has_permissions(manage_nicknames=True)
-    @commands.command(name="zalgocheck")
+    @commands.command(name="zalgocheck", hidden=True)
     async def zalgocheck(self, ctx: commands.Context, nick: str = None):
         """
         Mass remove zalgo names
+
+        This is hidden because it's less tested than the event based filter.
         """
 
         if nick is None:
             nick = await self.config.guild(ctx.guild).rename_to()
 
+        found_count = 0
         async for to_rename in self._filter_by_zalgo(ctx.guild.members):
             await to_rename.edit(nick=nick)
+            found_count += 1
+            
+        if found_count > 0:
+            await ctx.send(f"Found {found_count} users with zalgo names and renamed them")
+        else:
+            await ctx.send(
+                "Hey, your members either aren't assholes, or the autofilter already caught them."
+            )
