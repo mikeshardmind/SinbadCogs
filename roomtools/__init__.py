003860a361de6e7bea3ba63db859ad8c20442158
Michael H <michael@michaelhall.tech>
2018-08-18 17:12:01 -0400

    .

--- roomtools/__init__.py
+++ roomtools/__init__.py
@@ -12,7 +12,7 @@ class RoomTools(AutoRooms, TempChannels):
     """
 
     __author__ = "mikeshardmind"
-    __version__ = "7.0.0"
+    __version__ = "7.0.1"
 
     antispam_intervals = [
         (timedelta(seconds=5), 3),
