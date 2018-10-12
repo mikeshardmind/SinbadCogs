1a80607c71cb407975e21a840342dd75b3a711fe
Michael Hall <michael@michaelhall.tech>
2018-10-12 00:01:45 -0400

    .

--- modreplacer/__init__.py
+++ modreplacer/__init__.py
@@ -1,7 +1,13 @@
 from redbot.core.bot import Red
 from .mod import Mod
 
+try:
+    from redbot.core.errors import CogLoadError
+except ImportError:
+    CogLoadError = RuntimeError
 
-async def setup(bot: Red):
-    await bot.get_cog("Core")._unload("Mod")
+
+def setup(bot: Red):
+    if "Mod" in bot.cogs.keys():
+        raise CogLoadError("This replaces the mod cog. Unload it first if you want this.")
     bot.add_cog(Mod(bot))
