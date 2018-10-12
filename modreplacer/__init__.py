9f1c68fd3b9d5d26cc8600de221d1608c4a98b6a
Michael Hall <michael@michaelhall.tech>
2018-10-11 23:50:57 -0400

    fix that

--- modreplacer/__init__.py
+++ modreplacer/__init__.py
@@ -2,6 +2,6 @@ from redbot.core.bot import Red
 from .mod import Mod
 
 
-def setup(bot: Red):
-    bot.get_cog("Core")._unload("Mod")
+async def setup(bot: Red):
+    await bot.get_cog("Core")._unload("Mod")
     bot.add_cog(Mod(bot))
