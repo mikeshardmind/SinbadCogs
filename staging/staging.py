306ff50284ec963e6e612f683dd8d0b1af052753
Michael Hall <michael@michaelhall.tech>
2018-12-02 23:49:44 -0500

    shut up the typechecker, + a fix

--- staging/staging.py
+++ staging/staging.py
@@ -36,9 +36,10 @@ class Staging(commands.Cog):
             return
         try:
             _id = user.id  # type: ignore
+            user_obj: discord.User = user
         except AttributeError:
             _id = user
-            user = self.bot.get_user(_id)
+            user_obj = self.bot.get_user(_id)
 
         is_owner = await ctx.bot.is_owner(ctx.author)
 
@@ -47,13 +48,13 @@ class Staging(commands.Cog):
                 self.user_finder_as[ctx.author] = AntiSpam(antispam_intervals)
                 self.user_finder_as[ctx.author].stamp()
 
-        if not (user or is_owner):
+        if not (user_obj or is_owner):
             return await ctx.send(_("I couldn't locate that user in shared servers"))
         elif not user:
             try:
-                user = await self.bot.get_user_info(_id)
+                user_obj = await self.bot.get_user_info(_id)
             except discord.NotFound:
-                await ctx.send(_("No such user."))
+                return await ctx.send(_("No such user."))
             except discord.HTTPException:
                 await ctx.send(
                     _("Something unexpected prevented this user from being found.")
@@ -68,15 +69,15 @@ class Staging(commands.Cog):
                     _("I couldn't locate that user in shared servers")
                 )
 
-        since_created = (ctx.message.created_at - user.created_at).days
-        user_created = user.created_at.strftime("%d %b %Y %H:%M")
+        since_created = (ctx.message.created_at - user_obj.created_at).days
+        user_created = user_obj.created_at.strftime("%d %b %Y %H:%M")
         created_on = _("{}\n({} days ago)").format(user_created, since_created)
 
         data = discord.Embed(colour=(await ctx.embed_color()))
         data.add_field(name=_("Joined Discord on"), value=created_on)
-        name = str(user)
-        if user.avatar:
-            avatar = user.avatar_url_as(static_format="png")
+        name = str(user_obj)
+        if user_obj.avatar:
+            avatar = user_obj.avatar_url_as(static_format="png")
             data.set_author(name=name, url=avatar)
             data.set_thumbnail(url=avatar)
         else:
