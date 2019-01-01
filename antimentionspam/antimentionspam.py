62e7ea026fd94f5adf709abaa91d95e5222c51c6
Michael Hall <michael@michaelhall.tech>
2019-01-01 13:11:31 -0500

    [AntiMentionSpam] More options
    This is a pretty large set of changes, and some behavior defaults have
    changed.
    
    Adds support for a warning message, mutes, and now single message
    infractions do not trigger a ban by default unless also hitting the
    threshold intervals set.
    
    resolves #102

--- antimentionspam/antimentionspam.py
+++ antimentionspam/antimentionspam.py
@@ -1,4 +1,3 @@
-from typing import Any
 from datetime import timedelta
 
 import discord
@@ -11,7 +10,8 @@ class AntiMentionSpam(commands.Cog):
     """removes mass mention spam"""
 
     __author__ = "mikeshardmind (Sinbad)"
-    __version__ = "1.2.1"
+    __version__ = "1.3.0"
+    __flavor_text__ = "The version with more options for handling people."
 
     def __init__(self, bot):
         self.bot = bot
@@ -19,7 +19,13 @@ class AntiMentionSpam(commands.Cog):
             self, identifier=78631113035100160, force_registration=True
         )
         self.config.register_guild(
-            max_mentions=0, threshold=0, interval=0, autoban=False
+            max_mentions=0,
+            threshold=0,
+            interval=0,
+            autoban=False,
+            mute=False,
+            warnmsg="",
+            ban_single=False,
         )
         self.antispam = {}
 
@@ -47,8 +53,9 @@ class AntiMentionSpam(commands.Cog):
     async def set_max_interval_mentions(
         self, ctx: commands.Context, number: int, seconds: int
     ):
-        """sets the maximum number of mentions allowed in a time period.
-        
+        """
+        sets the maximum number of mentions allowed in a time period.
+
         (0 for both to remove setting)
         """
         await self.config.guild(ctx.guild).threshold.set(number)
@@ -74,13 +81,50 @@ class AntiMentionSpam(commands.Cog):
 
         await ctx.send(f"Autoban mention spammers: {enabled}")
 
+    @checks.admin_or_permissions(manage_guild=True)
+    @antimentionspam.command(name="warnmsg")
+    async def warnmessage(self, ctx, *, msg: str):
+        """
+        Sets the warn message. a message of "clear" turns the warn message off
+        """
+
+        if msg.lower() == "clear":
+            msg = ""
+        await self.config.guild(ctx.guild).warnmsg.set(msg)
+        await ctx.tick
+
+    @checks.admin_or_permissions(manage_guild=True)
+    @antimentionspam.command(name="singlebantoggle")
+    async def singlebantog(self, ctx, enabled: bool = None):
+        """
+        Sets if single message limits allow a ban
+
+        Default: False (interval threshold exceeding is required)
+        """
+        if enabled is None:
+            enabled = not await self.config.guild(ctx.guild).ban_single()
+        await self.config.guild(ctx.guild).ban_single.set(enabled)
+        await ctx.send(f"Ban from single message settings: {enabled}")
+
+    @checks.admin_or_permissions(manage_guild=True)
+    @antimentionspam.command(name="mutetoggle")
+    async def mute_toggle(self, ctx, enabled: bool = None):
+        """
+        Sets if a mute should be applied on exceeding limits set.
+        """
+
+        if enabled is None:
+            enabled = not await self.config.guild(ctx.guild).mute()
+        await self.config.guild(ctx.guild).mute.set(enabled)
+        await ctx.send(f"Ban from single message settings: {enabled}")
+
     async def process_intervals(self, message: discord.Message):
         guild = message.guild
         author = message.author
 
-        s = await self.config.guild(guild).all()
+        data = await self.config.guild(guild).all()
 
-        thresh, secs, ban = s["threshold"], s["interval"], s["autoban"]
+        thresh, secs = data["threshold"], data["interval"]
 
         if not (thresh > 0 and secs > 0):
             return
@@ -99,19 +143,53 @@ class AntiMentionSpam(commands.Cog):
         if self.antispam[guild.id][author.id].spammy:
             if await self.is_immune(message):
                 return
-            if ban:
-                try:
-                    await guild.ban(discord.Object(id=author.id), reason="mention spam")
-                except discord.HTTPException:
-                    pass
-                else:
-                    return True
+            await self.maybe_punish(message)
+            return True
+
+    async def maybe_punish(
+        self, message: discord.Message, single_message: bool = False
+    ):
+        guild = message.guild
+        target = message.author
+
+        data = await self.config.guild(guild).all()
+
+        ban = data["autoban"] and (data["ban_single"] or not single_message)
+        warnmsg = data["warnmsg"]
+        mute = data["mute"] and not ban
+
+        if ban and guild.me.guild_permissions.ban_members:
+            try:
+                return await guild.ban(
+                    discord.Object(id=target.id), reason="Mention Spam (Automated ban)"
+                )
+            except discord.HTTPException:
+                pass
+
+        if warnmsg and message.channel.permissions_for(guild.me).send_messages:
+            try:
+                await message.channel.send(f"{target.mention}: {warnmsg}")
+            except discord.HTTPException:
+                pass
+
+        if mute:
+            for channel in guild.text_channels:
+                if channel.permissions_for(guild.me).manage_roles:
+                    overwrites = channel.overwrites_for(target)
+                    overwrites.update(
+                        send_messages=False, add_reactions=False, administrator=False
+                    )
+                    try:
+                        await channel.set_permissions(
+                            target,
+                            overwrite=overwrites,
+                            reason="Mention Spam (Automated mute)",
+                        )
+                    except discord.HTTPException:
+                        pass
 
     async def on_message(self, message: discord.Message):
-        if not isinstance(message.channel, discord.TextChannel):
-            return
-
-        if message.author.bot:
+        if message.author.bot or message.guild is None:
             return
 
         guild = message.guild
@@ -119,26 +197,19 @@ class AntiMentionSpam(commands.Cog):
         channel = message.channel
         if not message.mentions:
             return
-        limit = await self.config.guild(guild).max_mentions()
-
-        if await self.process_intervals(message):
-            return  # banned already
-
-        if len(message.mentions) < limit or limit <= 0:
-            return
 
         if await self.is_immune(message):
             return
 
-        ban = await self.config.guild(guild).autoban()
+        limit = await self.config.guild(guild).max_mentions()
 
-        if ban:
-            try:
-                return await guild.ban(
-                    discord.Object(id=author.id), reason="mention spam"
-                )
-            except:
-                pass
+        if await self.process_intervals(message):
+            return  # already punished
+
+        if len(message.mentions) < limit or limit <= 0:
+            return
+
+        await self.maybe_punish(message, single_message=True)
 
         if not channel.permissions_for(guild.me).manage_messages:
             if channel.permissions_for(guild.me).send_messages:
@@ -150,7 +221,7 @@ class AntiMentionSpam(commands.Cog):
 
         try:
             await message.delete()
-        except:
+        except discord.HTTPException:
             if channel.permissions_for(guild.me).send_messages:
                 await channel.send(
                     f"Attempt to delete message from {author.mention} "
@@ -165,6 +236,10 @@ class AntiMentionSpam(commands.Cog):
 
     async def is_immune(self, message) -> bool:
         imset = getattr(self.bot, "is_automod_immune", self._is_immune)
+        author = message.author
+        guild = message.guild
+        if author == guild.owner or author.top_role >= guild.me.top_role:
+            return True  # Even if they aren't, actually immune, we don't care here.
         return await self.bot.is_owner(message.author) or await imset(message)
 
     async def _is_immune(self, message):
