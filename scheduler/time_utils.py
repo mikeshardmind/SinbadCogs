e1deeeedcc99da477b772d092223081f43c0f148
Michael Hall <michael@michaelhall.tech>
2019-01-01 09:58:05 -0500

    Still needs more work...

--- scheduler/time_utils.py
+++ scheduler/time_utils.py
@@ -1,5 +1,5 @@
-from datetime import datetime as dt
-
+from datetime import datetime as dt, timedelta
+from typing import Callable
 import pytz
 from dateutil.tz import gettz
 from dateutil import parser
@@ -22,3 +22,26 @@ def parse_time(datetimestring: str):
     ret = parser.parse(datetimestring, tzinfos=dict(gen_tzinfos()))
     ret = ret.astimezone(pytz.utc)
     return ret
+
+
+def td_format(td_object: timedelta, _: Callable = lambda x: x) -> str:
+    seconds = int(td_object.total_seconds())
+    periods = [
+        (_("year"), _("years"), 60 * 60 * 24 * 365),
+        (_("month"), _("months"), 60 * 60 * 24 * 30),
+        (_("day"), _("days"), 60 * 60 * 24),
+        (_("hour"), _("hours"), 60 * 60),
+        (_("minute"), _("minutes"), 60),
+        (_("second"), _("seconds"), 1),
+    ]
+
+    strings = []
+    for period_name, plural_period_name, period_seconds in periods:
+        if seconds > period_seconds:
+            period_value, seconds = divmod(seconds, period_seconds)
+            if period_value == 0:
+                continue
+            unit = plural_period_name if period_value > 1 else period_name
+            strings.append(f"{period_value} {unit}")
+
+    return ", ".join(strings)
