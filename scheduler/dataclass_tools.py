fbe34c087d5fcc72d9c45fb0737340abb92164a4
mikeshardmind <michael@michaelhall.tech>
2019-02-13 14:50:49 -0500

    Formatting
    Don't forget to format next time please @DiscordLiz

--- scheduler/dataclass_tools.py
+++ scheduler/dataclass_tools.py
@@ -10,21 +10,21 @@ def add_slots(cls):
     #  after a class has been created.
 
     # Make sure __slots__ isn't already set.
-    if '__slots__' in cls.__dict__:
-        raise TypeError(f'{cls.__name__} already specifies __slots__')
+    if "__slots__" in cls.__dict__:
+        raise TypeError(f"{cls.__name__} already specifies __slots__")
 
     # Create a new dict for our new class.
     cls_dict = dict(cls.__dict__)
     field_names = tuple(f.name for f in dataclasses.fields(cls))
-    cls_dict['__slots__'] = field_names
+    cls_dict["__slots__"] = field_names
     for field_name in field_names:
         # Remove our attributes, if present. They'll still be
         #  available in _MARKER.
         cls_dict.pop(field_name, None)
     # Remove __dict__ itself.
-    cls_dict.pop('__dict__', None)
+    cls_dict.pop("__dict__", None)
     # And finally create the class.
-    qualname = getattr(cls, '__qualname__', None)
+    qualname = getattr(cls, "__qualname__", None)
     cls = type(cls)(cls.__name__, cls.__bases__, cls_dict)
     if qualname is not None:
         cls.__qualname__ = qualname
