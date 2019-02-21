9ed49703afed641c18b93960aac7c47eee7244cf
Michael H <michael@michaelhall.tech>
2019-02-20 21:55:44 -0500

    Handled some of bansync.

--- bansync/bansync.py
+++ bansync/bansync.py
@@ -1,14 +1,21 @@
+# TODO:
+# cog level erro handling.
+# add logging
+# finish typehinting
+# add some docstrings.
+# I'm handling the remaining work on this --Michael
+
 import asyncio
-import json
 import io
-from typing import List, Set, TYPE_CHECKING, Union
+import json
+from typing import List, Set, TYPE_CHECKING, Union, AsyncIterator, Dict
 
 import discord
-
-from redbot.core.utils.chat_formatting import box, pagify
 from redbot.core import commands, checks
 from redbot.core.config import Config
 from redbot.core.i18n import Translator, cog_i18n
+from redbot.core.utils.chat_formatting import box, pagify
+
 from .converters import SyndicatedConverter
 
 if TYPE_CHECKING:
@@ -17,54 +24,9 @@ if TYPE_CHECKING:
 GuildList = List[discord.Guild]
 GuildSet = Set[discord.Guild]
 
-T_ = Translator("BanSync", __file__)
-_ = lambda s: s  # So that unguarded strings below are not modified in place.
-
-# Strings go here for ease of modification with pygettext
-INTERACTIVE_PROMPT_I = _(
-    "Select a server to add to the sync list by number, "
-    'or enter "-1" to stop adding servers'
-)
-
-ASYNCIOTIMEOUT = _("You took too long, try again later")
-
-INVALID_CHOICE = _("That wasn't a valid choice")
-
-TOO_FEW_CHOSEN = _("I need at least two servers to sync")
-
-BAN_REASON = _("Ban synchronization")
-
-BANS_SYNCED = _("Bans have been synchronized across selected servers")
-
-UNWORTHY = _("You are not worthy")
-
-PARTIAL_SUCCESS = _(
-    "I got some of those, but other's couldn't be banned for some reason."
-)
-
-TOO_MANY_BANS_WAT = _(
-    "Contact me. You have a very interesting ban list to be too large to send."
-)
-
-EXPECTED_FILE = _(
-    "You definitely need to supply me an exported ban list to be imported."
-)
-
-INVALID_FILE = _("That wasn't an exported ban list")
-
-ALL_ALREADY_BANNED = _("That list doesn't contain anybody not already banned.")
-
-ALREADY_EXCLUDED = _("This server has already opted out of these actions.")
-
-NOT_EXCLUDED = _("This server has not opted out of these actions.")
-
-BANMOJI = "\U0001f528"
-
-_ = T_
+_ = Translator("BanSync", __file__)
 
 
-# pylint: disable=E1133
-# pylint doesn't seem to handle implied async generators properly yet
 @cog_i18n(_)
 class BanSync(commands.Cog):
     """
@@ -73,21 +35,22 @@ class BanSync(commands.Cog):
 
     __version__ = "2.0.0"
 
-    def __init__(self, bot):
+    def __init__(self, bot: "Red"):
         self.bot: "Red" = bot
         self.config = Config.get_conf(self, identifier=78631113035100160)
         self.config.register_global(excluded_from_automatic=[])
 
     @commands.group()
-    async def bansyncset(self, ctx):
+    async def bansyncset(self, ctx: commands.Context):
         """
         Options for bansync
         """
+        pass
 
     @checks.guildowner_or_permissions(administrator=True)
     @commands.guild_only()
     @bansyncset.command()
-    async def automaticoptout(self, ctx):
+    async def automaticoptout(self, ctx: commands.Context):
         """
         This allows you to opt a server out of being selected for some actions
 
@@ -104,14 +67,16 @@ class BanSync(commands.Cog):
         """
         async with self.config.excluded_from_automatic() as exclusions:
             if ctx.guild.id in exclusions:
-                return await ctx.send(_(ALREADY_EXCLUDED))
+                return await ctx.send(
+                    _("This server has already opted out of these actions.")
+                )
             exclusions.append(ctx.guild.id)
         await ctx.tick()
 
     @checks.guildowner_or_permissions(administrator=True)
     @commands.guild_only()
     @bansyncset.command()
-    async def automaticoptin(self, ctx):
+    async def automaticoptin(self, ctx: commands.Context):
         """
         This allows you to opt back into certain automatic actions.
 
@@ -119,7 +84,9 @@ class BanSync(commands.Cog):
         """
         async with self.config.excluded_from_automatic() as exclusions:
             if ctx.guild.id not in exclusions:
-                return await ctx.send(_(NOT_EXCLUDED))
+                return await ctx.send(
+                    _("This server has not opted out of these actions.")
+                )
             exclusions.remove(ctx.guild.id)
         await ctx.tick()
 
@@ -127,7 +94,7 @@ class BanSync(commands.Cog):
     @checks.admin_or_permissions(ban_members=True)
     @commands.guild_only()
     @commands.command(name="exportbans")
-    async def exportbans(self, ctx):
+    async def exportbans(self, ctx: commands.Context):
         """
         Exports current servers bans to json
         """
@@ -145,19 +112,27 @@ class BanSync(commands.Cog):
                 ctx.author.mention, files=[discord.File(fp, filename=filename)]
             )
         except discord.HTTPException:
-            await ctx.send(_(TOO_MANY_BANS_WAT))
+            await ctx.send(
+                (
+                    "You have a very interesting ban list to be too large to send, open an issue."
+                )
+            )
 
     @commands.bot_has_permissions(ban_members=True)
     @checks.admin_or_permissions(ban_members=True)
     @commands.guild_only()
     @commands.command(name="importbans")
-    async def importbans(self, ctx):
+    async def importbans(self, ctx: commands.Context):
         """
         Imports bans from json
         """
 
         if not ctx.message.attachments:
-            return await ctx.send(_(EXPECTED_FILE))
+            return await ctx.send(
+                _(
+                    "You definitely need to supply me an exported ban list to be imported."
+                )
+            )
 
         fp = io.BytesIO()
         a = ctx.message.attachments[0]
@@ -167,13 +142,15 @@ class BanSync(commands.Cog):
             assert isinstance(data, list)
             assert all(isinstance(x, int) for x in data)
         except (json.JSONDecodeError, AssertionError):
-            return await ctx.send(_(INVALID_FILE))
+            return await ctx.send(_("That wasn't an exported ban list"))
 
         current_bans = await ctx.guild.bans()
         to_ban = set(data) - {b.user.id for b in current_bans}
 
         if not to_ban:
-            return await ctx.send(_(ALL_ALREADY_BANNED))
+            return await ctx.send(
+                _("That list doesn't contain anybody not already banned.")
+            )
 
         async with ctx.typing():
             exit_codes = [
@@ -187,14 +164,18 @@ class BanSync(commands.Cog):
             ]
 
         if all(exit_codes):
-            await ctx.message.add_reaction(BANMOJI)
+            await ctx.message.add_reaction("\N{HAMMER}")
         elif not any(exit_codes):
-            await ctx.send(_(UNWORTHY))
+            await ctx.send(_("You are not worthy"))
         else:
-            await ctx.send(_(PARTIAL_SUCCESS))
+            await ctx.send(
+                _(
+                    "I got some of those, but other's couldn't be banned for some reason."
+                )
+            )
 
     @commands.command(name="bulkban")
-    async def bulkban(self, ctx, *ids: int):
+    async def bulkban(self, ctx: commands.Context, *ids: int):
         """
         bulk global bans by id
         """
@@ -203,23 +184,17 @@ class BanSync(commands.Cog):
             results = {i: await self.targeted_global_ban(ctx, i, rsn) for i in set(ids)}
 
         if all(results.values()):
-            await ctx.message.add_reaction(BANMOJI)
+            await ctx.message.add_reaction("\N{HAMMER}")
         elif not any(results.values()):
-            await ctx.send(_(UNWORTHY))
+            await ctx.send(_("You are not worthy"))
         else:
-            await ctx.send(_(PARTIAL_SUCCESS))
-
-    @commands.command(name="bansyncdebuginfo", hidden=True)
-    async def debuginfo(self, ctx):
-        await ctx.send(
-            box(
-                "Author: {a}\nBan Sync Version: {ver} ({flavor})".format(
-                    a=self.__author__, ver=self.__version__, flavor=self.__flavor_text__
+            await ctx.send(
+                _(
+                    "I got some of those, but other's couldn't be banned for some reason."
                 )
             )
-        )
 
-    async def can_sync(self, guild: discord.Guild, mod: discord.User):
+    async def can_sync(self, guild: discord.Guild, mod: discord.User) -> bool:
         user_allowed = False
         user_allowed |= await self.bot.is_owner(mod)
         mod = guild.get_member(mod.id)
@@ -234,14 +209,14 @@ class BanSync(commands.Cog):
 
     async def ban_filter(
         self, guild: discord.Guild, mod: discord.user, target: discord.user
-    ):
-        is_owner = await self.bot.is_owner(mod)
+    ) -> bool:
+        is_owner: bool = await self.bot.is_owner(mod)
 
         mod = guild.get_member(mod.id)
         if mod is None and not is_owner:
             return False
 
-        can_ban = guild.me.guild_permissions.ban_members
+        can_ban: bool = guild.me.guild_permissions.ban_members
         if not is_owner:
             can_ban &= mod.guild_permissions.ban_members
 
@@ -254,9 +229,9 @@ class BanSync(commands.Cog):
 
     async def ban_or_hackban(
         self, guild: discord.Guild, _id: int, *, mod: discord.User, reason: str = None
-    ):
+    ) -> bool:
         member = guild.get_member(_id)
-        reason = reason or BAN_REASON
+        reason = reason or _("Ban synchronization")
         if member is None:
             member = discord.Object(id=_id)
         if not await self.ban_filter(guild, mod, member):
@@ -265,14 +240,17 @@ class BanSync(commands.Cog):
             await guild.ban(member, reason=reason, delete_message_days=0)
         except discord.HTTPException:
             return False
-        else:
-            return True
 
-    async def guild_discovery(self, ctx: commands.context, picked: GuildSet):
+        return True
+
+    async def guild_discovery(
+        self, ctx: commands.context, picked: GuildSet
+    ) -> AsyncIterator[discord.Guild]:
         for g in sorted(self.bot.guilds, key=lambda s: s.name):
             if g not in picked and await self.can_sync(g, ctx.author):
                 yield g
 
+    # TODO : restructure how this func works and interacts.
     async def interactive(self, ctx: commands.context, picked: GuildSet):
         output = ""
         guilds = [g async for g in self.guild_discovery(ctx, picked)]
@@ -280,7 +258,10 @@ class BanSync(commands.Cog):
             return -1
         for i, guild in enumerate(guilds, 1):
             output += "{}: {}\n".format(i, guild.name)
-        output += _(INTERACTIVE_PROMPT_I)
+        output += _(
+            "Select a server to add to the sync list by number, "
+            'or enter "-1" to stop adding servers'
+        )
         for page in pagify(output, delims=["\n"]):
             await ctx.send(box(page))
 
@@ -299,7 +280,7 @@ class BanSync(commands.Cog):
                 else:
                     guild = guilds[message - 1]
             except (ValueError, IndexError):
-                await ctx.send(_(INVALID_CHOICE))
+                await ctx.send(_("That wasn't a valid choice"))
                 return None
             else:
                 return guild
@@ -311,16 +292,16 @@ class BanSync(commands.Cog):
         sources: GuildSet,
         dests: GuildSet,
         auto: bool = False,
-    ):
+    ) -> None:
 
-        bans: dict = {}
-        banlist = set()
+        bans: Dict[int, Set[discord.User]] = {}
+        banlist: Set[discord.User] = set()
 
         if auto:
-            exclusions = await self.config.excluded_from_automatic()
+            exclusions: List[int] = await self.config.excluded_from_automatic()
             dests = {g for g in dests if g.id not in exclusions}
 
-        guilds = sources | dests
+        guilds: GuildSet = sources | dests
 
         for guild in guilds:
             bans[guild.id] = set()
@@ -338,7 +319,7 @@ class BanSync(commands.Cog):
             for maybe_ban in to_ban:
                 if await self.ban_filter(guild, usr, maybe_ban):
                     await self.ban_or_hackban(
-                        guild, maybe_ban.id, mod=usr, reason=_(BAN_REASON)
+                        guild, maybe_ban.id, mod=usr, reason=_("Ban synchronization")
                     )
 
     @commands.command(name="bansync")
@@ -346,22 +327,19 @@ class BanSync(commands.Cog):
         """
         syncs bans across servers
         """
+        guilds: GuildSet = set()
         if not auto:
-            guilds: GuildSet = set()
             while True:
                 s = await self.interactive(ctx, guilds)
                 if s == -1:
                     break
                 if s == -2:
-                    return await ctx.send(_(ASYNCIOTIMEOUT))
+                    return await ctx.send(_("You took too long, try again later"))
                 elif s is None:
                     continue
                 else:
                     guilds.add(s)
         elif auto is True:
-            # I'm removing the exclusions here differently than syndicate
-            # I'd have to redesign a large portion of your converters or
-            # Have poorly defined behavior on automatic bansyncs otherwise. --Liz
             exclusions = await self.config.excluded_from_automatic()
             guilds: GuildSet = {
                 g
@@ -369,9 +347,8 @@ class BanSync(commands.Cog):
                 if g.id not in exclusions and await self.can_sync(g, ctx.author)
             }
 
-        # noinspection PyUnboundLocalVariable
         if len(guilds) < 2:
-            return await ctx.send(_(TOO_FEW_CHOSEN))
+            return await ctx.send(_("I need at least two servers to sync"))
 
         async with ctx.typing():
             await self.process_sync(usr=ctx.author, sources=guilds, dests=guilds)
@@ -401,20 +378,18 @@ class BanSync(commands.Cog):
         """
         banned = await self.targeted_global_ban(ctx, user, rsn)
         if banned:
-            await ctx.message.add_reaction(BANMOJI)
+            await ctx.message.add_reaction("\N{HAMMER}")
         else:
-            await ctx.send(_(UNWORTHY))
+            await ctx.send(_("You are not worthy"))
 
     async def targeted_global_ban(
         self, ctx: commands.Context, user: Union[discord.Member, int], rsn: str = None
-    ):
+    ) -> bool:
         """
         This respects the exclusions in config
         """
-        try:
-            _id: int = user.id
-        except AttributeError:
-            _id: int = user
+
+        _id: int = getattr(user, "id", user)
 
         excluded: GuildSet = {
             g
--- bansync/converters.py
+++ bansync/converters.py
@@ -1,17 +1,26 @@
-import shlex
 import argparse
+import shlex
+from typing import Dict, Any
 
 from redbot.core.commands import Converter, Context, BadArgument
+from redbot.core.i18n import Translator
+
+_ = Translator("BanSync", __file__)
 
 
 class NoExitParser(argparse.ArgumentParser):
+    """By default, an error on this calls sys.exit"""
+
     def error(self, message):
-        raise BadArgument()
+        raise BadArgument() from None
 
 
 class SyndicatedConverter(Converter):
     """
-    Handler for this
+    Parser based converter.
+
+    Takes sources, and either
+        destinations, a flag to automatically determine destinations, or both
     """
 
     async def convert(self, ctx: Context, argument: str) -> dict:
@@ -24,13 +33,13 @@ class SyndicatedConverter(Converter):
         )
 
         vals = parser.parse_args(shlex.split(argument))
-        ret = {}
+        ret: Dict[str, Any] = {}
 
         guilds = set(ctx.bot.guilds)
 
         ret["sources"] = set(filter(lambda g: str(g.id) in vals.sources, guilds))
         if not ret["sources"]:
-            raise BadArgument("I need at least 1 source.")
+            raise BadArgument(_("I need at least 1 source.")) from None
 
         if vals.auto:
             ret["dests"] = guilds - ret["sources"]
@@ -43,7 +52,11 @@ class SyndicatedConverter(Converter):
                     ret["dests"].add(guild)
         else:
             raise BadArgument(
-                "I need either at least one destination, or to be told to try everywhere not a source."
-            )
+                _(
+                    "I need either at least one destination, "
+                    " to be told to automatically determine destinations, "
+                    "or a combination of both to add extra destinations beyond the automatic."
+                )
+            ) from None
         ret["usr"] = ctx.author
         return ret
