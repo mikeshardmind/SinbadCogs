a9c82b5540b5d50747926db5cd3f8760768169b0
Michael H <michael@michaelhall.tech>
2019-03-29 23:06:55 -0400

    sigh

--- filter/__init__.py
+++ filter/__init__.py
@@ -4,11 +4,4 @@ from redbot.core import commands
 
 def setup(bot):
     filter_cog = Filter(bot)
-    # This won't be needed after 3.1 of Red
-    for attr_name in dir(filter_cog):
-        x = getattr(filter_cog, attr_name, None)
-        if isinstance(x, commands.Command):
-            x.instance = filter_cog
-            # setattr(filter_cog, attr_name, x)
-
     bot.add_cog(filter_cog)
