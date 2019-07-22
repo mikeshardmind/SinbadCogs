71824e598a777e852af939b99c4a4b7f094954af
Michael H <michael@michaelhall.tech>
2019-07-22 16:26:38 -0400

    fix the shared cog

--- sinbad_libs/tools.py
+++ sinbad_libs/tools.py
@@ -12,8 +12,6 @@ from redbot import version_info
 class ToolBox(commands.Cog, name="Sinbad's Toolbox"):
     def __init__(self, bot):
         self.bot = bot
-        self.sent = False
-        self.active = False
         self._loops = asyncio.create_task(self.do_stuff())
 
     async def do_stuff(self):
@@ -21,34 +19,31 @@ class ToolBox(commands.Cog, name="Sinbad's Toolbox"):
         await self.bot.wait_until_ready()
         while True:
             await asyncio.sleep(300)
-            if not self.sent:
-                self.active = True
-            else:
-                self.sent = False
-
-    async def bot_check(self, ctx):
-        if "DYNO" in os.environ:
-            if await self.do_token_check():
+            if self.is_bad_user():
                 with contextlib.supress(Exception):
                     if self.active and not self.sent:
                         await self.bot.send_to_owners(
                             "Go reset your token and stop hosting on "
                             "Heroku if you can't be bothered to do it without leaking a token."
                         )
-                        self.sent = True
+
                 with contextlib.suppress(Exception):
                     g = discord.Game(
                         name="This bot's token appears to have been leaked online."
                     )
                     await self.bot.change_presence(game=g)
+            else:
+                raise asyncio.CancelledError()
 
-                return False
+    async def bot_check(self, ctx):
+        if self.is_bad_user():
+            return False
         return True
 
-    async def do_token_check(self):
+    def is_bad_user(self):
         """
         We only care if someone is hosting irresponsibly,
         which lets face it, is almost everyone hosting Red on heroku
         """
-        if os.getenv("RED_TOKEN") != bot.http.token:
+        if "DYNO" in os.environ and os.getenv("RED_TOKEN") != self.bot.http.token:
             return True
