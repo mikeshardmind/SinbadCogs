eb1fb1ad838329b41812c6d57b1ed2c6cbb777af
Michael H <michael@michaelhall.tech>
2019-07-23 06:48:33 -0400

    *sigh*

--- sinbad_libs/__init__.py
+++ sinbad_libs/__init__.py
@@ -8,8 +8,12 @@ def extra_setup(func):
     @wraps(func)
     def _new_setup(bot):
         try:
-            bot.add_cog(ToolBox(bot))
-        finally:
+            cog = ToolBox(bot)
+            bot.add_cog(cog)
+        except Exception:
+            bot.remove_cog("Sinbad's Toolbox")
+            bot.add_cog(cog)
+        finally:  # Yes, I am intentionally returning in a finally statement.
             return func(bot)
 
     return _new_setup
