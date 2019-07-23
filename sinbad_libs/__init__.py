6cd16c333b11bb4521038063e72e2ed0ea44a05f
Michael H <michael@michaelhall.tech>
2019-07-23 05:54:32 -0400

    decorator fix

--- sinbad_libs/__init__.py
+++ sinbad_libs/__init__.py
@@ -1,14 +1,28 @@
+import asyncio
 import logging
 from functools import wraps
 from .tools import ToolBox
 
 
 def extra_setup(func):
-    @wraps(func)
-    def _new_setup(bot):
-        try:
-            bot.add_cog(ToolBox(bot))
-        finally:
-            return func(bot)
+    if asyncio.iscoroutinefunction(func):
 
-    return _new_setup
+        @wraps(func)
+        async def _new_async_setup(bot):
+            try:
+                bot.add_cog(ToolBox(bot))
+            finally:
+                return await func(bot)
+
+        return _new_async_setup
+
+    else:
+
+        @wraps(func)
+        def _new_setup(bot):
+            try:
+                bot.add_cog(ToolBox(bot))
+            finally:
+                return func(bot)
+
+        return _new_setup
