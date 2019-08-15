a4a612742af0730bc4a03ceb7875aeca091f8e75
Michael H <michael@michaelhall.tech>
2019-08-14 22:21:14 -0400

    god bless importlib

--- sinbad_libs/__init__.py
+++ sinbad_libs/__init__.py
@@ -1,16 +1,16 @@
 import asyncio
+import importlib
 import logging
 from functools import wraps
-from .tools import ToolBox
+from . import tools
 
 
 def extra_setup(func):
     @wraps(func)
     def _new_setup(bot):
         try:
-            cog = ToolBox(bot)
-            bot.add_cog(cog)
-        except Exception:
+            module = importlib.reload(tools)
+            cog = module.ToolBox(bot)
             bot.remove_cog("Sinbad's Toolbox")
             bot.add_cog(cog)
         finally:  # Yes, I am intentionally returning in a finally statement.
