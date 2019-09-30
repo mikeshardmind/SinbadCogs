788207f86a63813e8cf246f56042e95ab7f08dd9
Michael H <michael@michaelhall.tech>
2019-09-30 15:12:44 -0400

    Revert hot-reload changes

--- embedmaker/__init__.py
+++ embedmaker/__init__.py
@@ -1,11 +1,7 @@
-import importlib
-from . import embedmaker, serialize, yaml_parse
+from . import embedmaker
 from cog_shared.sinbad_libs import extra_setup
 
 
 @extra_setup
 def setup(bot):
-    importlib.reload(serialize)
-    importlib.reload(yaml_parse)
-    embedmaker = embedmaker.reload(yaml_parse)
     bot.add_cog(embedmaker.EmbedMaker(bot))
--- mod/__init__.py
+++ mod/__init__.py
@@ -1,10 +1,9 @@
-import importlib
-from . import mod
+import discord
+from .mod import Mod
 from cog_shared.sinbad_libs import extra_setup
 
 
 @extra_setup
 def setup(bot):
-    mod = importlib.reload(mod)
-    cog = mod.Mod(bot)
+    cog = Mod(bot)
     bot.add_cog(cog)
