fd65bc03f0e909e3559c14a9b02adcc9a55dc251
Michael H <michael@michaelhall.tech>
2019-10-26 08:30:49 -0400

    ...

--- statuswarn/statuswarn.py
+++ statuswarn/statuswarn.py
@@ -1,6 +1,6 @@
 import discord  # type: ignore
 from redbot.core import checks
-from redbot.core.commands import command, Cog, guild_only
+from redbot.core.commands import command, group, Cog, guild_only
 from redbot.core.config import Config
 from redbot.core.bot import Red
 from redbot.core.utils.common_filters import INVITE_URL_RE
@@ -15,12 +15,16 @@ class StatusWarn(Cog):
         self.bot: Red = bot
         self.config = Config.get_conf(self, identifier=78631113035100160)
         self.config.register_guild(channel=None)
+        self.custom_message = None
 
     @Cog.listener(name="on_member_update")
     async def catcher(self, before, after):
         """
         Hmm?
         """
+
+        if before.activities == after.activities:
+            return
 
         maybe_custom = next(filter(lambda a: a.type == 4, after.activities), None)
         if not maybe_custom:
@@ -31,29 +35,34 @@ class StatusWarn(Cog):
             warn_channel_id = await self.config.guild(after.guild).channel()
             warn_channel = after.guild.get_channel(warn_channel_id)
             if warn_channel:
+                msg = self.custom_message or "Warning: detected invite url in status:"
                 await self.bot.send_filtered(
                     warn_channel,
                     filter_mass_mentions=True,
                     filter_all_links=False,
                     filter_invite_links=False,
-                    content=(
-                        f"Warning: {after.mention} has an invite url in status:"
-                        f"\n\n{maybe_custom.state}"
-                    ),
+                    content=f"{msg}\n\n{after.mention} {maybe_custom.state}",
                 )
 
     @checks.admin_or_permissions(manage_guild=True)
     @guild_only()
-    @command(name="urlstatuslogset")
+    @group(name="statuswarn")
     async def setter(self, ctx, channel: discord.TextChannel = None):
         """
-        Sets or clears the channel
+        configuration for statuswarn
         """
-        if not channel:
-            await self.config.guild(ctx.guild).channel.clear()
-            await ctx.send("Not logging (use with a channel to set)")
-        else:
-            await self.config.guild(ctx.guild).channel.set(channel.id)
-            await ctx.send(
-                f"Logging custom statuses with invitelinks to : {channel.mention}"
-            )
+        pass
+
+    @setter.command(name="setchannel")
+    async def set_channel(self, ctx, channel: discord.TextChannel):
+        """ Set the log channel """
+        await self.config.guild(ctx.guild).channel.set(channel.id)
+        await ctx.send(
+            f"Logging custom statuses with invitelinks to : {channel.mention}"
+        )
+
+    @setter.command(name="unsetchannel")
+    async def clear_channel(self, ctx):
+        """ remove the logging channel """
+        await self.config.guild(ctx.guild).channel.clear()
+        await ctx.send("Not logging (use with a channel to set)")
