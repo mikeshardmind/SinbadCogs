6e64438808fee40e2f0de319c7af9d60b338ccf1
Michael H <michael@michaelhall.tech>
2019-12-13 21:32:01 -0500

    use some contextmanagers for future sanity

---
+++ modnotes/aspw_wrapper.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+"""
+This should be moved into a pip installable lib at some point, but I'm being
+lazy in the short term here.
+"""
+import asyncio
+import queue
+
+from contextlib import contextmanager
+from os import PathLike
+from threading import Thread
+from typing import Generator, Union, TYPE_CHECKING
+
+import apsw
+
+if TYPE_CHECKING:
+    from typing_extensions import Protocol
+else:
+    Protocol = object
+
+
+class ProvidesCursor(Protocol):
+    def cursor(self) -> apsw.Cursor:
+        ...
+
+
+class ContextManagerMixin(ProvidesCursor):
+    @contextmanager
+    def with_cursor(self) -> Generator[apsw.Cursor, None, None]:
+        c = self.cursor()
+        try:
+            yield c
+        finally:
+            c.close()
+
+    @contextmanager
+    def transaction(self) -> Generator[apsw.Cursor, None, None]:
+        c = self.cursor()
+        try:
+            c.execute("BEGIN TRANSACTION")
+            yield c
+        except Exception:
+            c.execute("ROLLBACK TRANSACTION")
+        else:
+            c.execute("COMMIT TRANSACTION")
+        finally:
+            c.close()
+
+
+class Connection(apsw.Connection, ContextManagerMixin):
+    pass
+
+
+# TODO: asyncio friendly ThreadedConnection class
