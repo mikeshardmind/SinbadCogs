ea392b0eaca1d8837a7d382b72922a166ff0e8d6
Michael H <michael@michaelhall.tech>
2019-12-19 10:50:13 -0500

    .

--- mlog/todo_queries.py
+++ mlog/todo_queries.py
@@ -5,7 +5,6 @@ WITH last_edits AS (
     )
     WHERE dr=1
 )
-
 SELECT
     messages.message_id as mid,
     messages.author_id as aid,
@@ -15,4 +14,16 @@ SELECT
 FROM messages LEFT OUTER JOIN last_edits ON messages.message_id=last_edits.message_id
 WHERE messages.channel_id = ?
 ORDER BY created_at
+"""
+
+inactivy_metrics = """
+SELECT ar.author_id, ar.mc, DATETIME(ar.most_recent, "unixepoch")
+FROM (
+    SELECT COUNT(*) as mc, author_id, MAX(created_at) as most_recent
+    FROM messages WHERE guild_id = :guild_id
+    GROUP BY author_id
+) as ar
+WHERE ar.most_recent < COALESCE(:when, 0) OR ar.mc < COALESCE(:low, 0)
+ORDER BY ar.most_recent, ar.mc
+LIMIT COALESCE(:limit, -1)
 """
