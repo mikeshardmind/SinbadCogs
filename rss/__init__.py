c40104480280964cefdd3fa3f386282bce44d20c
Michael H <michael@michaelhall.tech>
2019-12-24 06:21:39 -0500

    just in case

--- rss/__init__.py
+++ rss/__init__.py
@@ -9,10 +9,18 @@ from cog_shared.sinbad_libs import extra_setup
 @extra_setup
 def setup(bot):
     try:
-        from .core import RSS
+        import feedparser
     except ImportError:
         raise CogLoadError(
             "You need `feedparser` for this. Downloader *should* have handled this for you."
         )
-    else:
-        bot.add_cog(RSS(bot))
+    try:
+        import discordtextsanitizer
+    except ImportError:
+        raise CogLoadError(
+            "You need `discord-text-sanitizer` for this. Downloader *should* have handled this for you."
+        )
+
+    from .core import RSS
+
+    bot.add_cog(RSS(bot))
