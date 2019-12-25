7eb73363566667cae9ce4279ef006a13f28a7ab9
Michael H <michael@michaelhall.tech>
2019-12-25 02:14:08 -0500

    Beginnings of something a bit more powerful

---
+++ templater/__init__.py
@@ -0,0 +1,12 @@
+from redbot.core.errors import CogLoadError
+
+from .core import Templater
+
+
+def setup(bot):
+    if bot.user.id != 275047522026913793:
+        raise CogLoadError(
+            "No. This one has security implications and "
+            "should not be loaded prior to me signing off on it being ready."
+        )
+    bot.add_cog(Templater(bot))
