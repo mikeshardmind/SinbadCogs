067158fe632401166aafc8b76fcfb5fef4ea4d9f
Michael H <michael@michaelhall.tech>
2019-12-25 02:14:08 -0500

    Beginnings of something a bit more powerful

---
+++ templater/core.py
@@ -0,0 +1,23 @@
+from redbot.core import commands, checks
+from redbot.core.utils.chat_formatting import pagify
+from .restrictedenvironment import RestrictedEnv
+
+
+class Templater(commands.Cog):
+    """ meh """
+
+    @checks.is_owner()
+    @commands.command()
+    async def tdebug(self, ctx: commands.Context, *, template: str):
+        """ This is probably a terrible idea """
+        e = RestrictedEnv(trim_blocks=True, lstrip_blocks=True)
+        e.globals = e.make_globals({"ctx": ctx})
+
+        t = e.from_string(template)
+        try:
+            result = t.render()
+        except Exception as exc:
+            result = str(type(exc))
+
+        for page in pagify(result):
+            await ctx.send(page)
