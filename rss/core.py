5744dc4358ee3471fa5b925cbe44a6e2a0d932f5
Michael H <michael@michaelhall.tech>
2020-01-10 23:26:48 -0500

    more work on this, I guess...

--- rss/core.py
+++ rss/core.py
@@ -192,7 +192,7 @@ class RSS(commands.Cog):
         escaped_usable_fields = {k: maybe_clean(k, v) for k, v in data.items() if v}
 
         content = template.safe_substitute(**escaped_usable_fields)
-        content = dts.preprocess_text(content, strip_html=True)
+        content = dts.sanitize_mass_mentions(content, strip_html=False, aggresive=True)
 
         if embed:
             if len(content) > 1980:
