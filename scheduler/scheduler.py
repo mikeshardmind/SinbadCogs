cba09652e9377221bf7d6fc0539cf78c35e38c4a
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-01-18 23:25:23 +0100

    Update scheduler.py (#194)

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -27,7 +27,7 @@ class Scheduler(commands.Cog):
     """
 
     __author__ = "mikeshardmind(Sinbad), DiscordLiz"
-    __version__ = "323.0.6"
+    __version__ = "323.0.7"
 
     def format_help_for_context(self, ctx):
         pre_processed = super().format_help_for_context(ctx)
@@ -49,7 +49,7 @@ class Scheduler(commands.Cog):
         self._iter_lock = asyncio.Lock()
 
     def init(self):
-        self.bot.loop.create_task(self.bg_loop())
+        self.bg_loop_task = self.bot.loop.create_task(self.bg_loop())
 
     def cog_unload(self):
         if self.bg_loop_task:
