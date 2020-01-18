576a50b3c48caf90ab0ee1f91d1c5a4876a74fb5
Michael H <michael@michaelhall.tech>
2020-01-18 00:41:36 -0500

    antimentionspam-mypy

---
+++ antimentionspam/commands.py
@@ -0,0 +1,5 @@
+from discord.ext.commands import guild_only
+from redbot.core.commands.commands import command, Cog, group
+from redbot.core.commands.context import Context
+
+#  Remove after resolution of GH: Cog-Creators/Red-DiscordBot#3407
