e4c1b61abaaca803a63506a53400d59ffc83f269
Michael H <michael@michaelhall.tech>
2020-01-20 18:59:44 -0500

    also, meh

--- bansync/commands.py
+++ bansync/commands.py
@@ -1,6 +1,28 @@
+from typing import TYPE_CHECKING as TYPE_CHECKING
+import discord as _discord
+
 from discord.ext.commands import guild_only, CommandError
 from redbot.core.commands.commands import command, Cog, group
 from redbot.core.commands.requires import bot_has_permissions
 from redbot.core.commands.context import Context
 
 #  Remove after resolution of GH: Cog-Creators/Red-DiscordBot#3407
+
+if TYPE_CHECKING:
+
+    class GuildContext(Context):
+        @property
+        def author(self) -> _discord.Member:
+            ...
+
+        @property
+        def channel(self) -> _discord.TextChannel:
+            ...
+
+        @property
+        def guild(self) -> _discord.Guild:
+            ...
+
+
+else:
+    GuildContext = Context
