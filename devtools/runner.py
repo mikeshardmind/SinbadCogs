e977c5b21813a3a12cc224acf21bc0968dffa27d
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-01-22 00:11:03 +0100

    [DevTools] Add venv's bin/Scripts folder to PATH for convenience (#193)
    * Update runner.py
    
    * Update runner.py

--- devtools/runner.py
+++ devtools/runner.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 import functools
 import io
+import os
 import subprocess  # nosec
+import sys
 from concurrent.futures import ThreadPoolExecutor
 
 import discord
@@ -25,7 +27,17 @@ class Runner(commands.Cog):
         self.executor.shutdown(wait=False)
 
     async def _run(self, command):
-
+        env = os.environ.copy()
+        if hasattr(sys, "real_prefix") or sys.base_prefix != sys.prefix:
+            # os.path.sep - this is folder separator, i.e. `\` on win or `/` on unix
+            # os.pathsep - this is paths separator in PATH, i.e. `;` on win or `:` on unix
+            # a wonderful idea to call them almost the same >.<
+            if sys.platform == "win32":
+                binfolder = f"{sys.prefix}{os.path.sep}Scripts"
+                env["PATH"] = f"{binfolder}{os.pathsep}{env['PATH']}"
+            else:
+                binfolder = f"{sys.prefix}{os.path.sep}bin"
+                env["PATH"] = f"{binfolder}{os.pathsep}{env['PATH']}"
         return (
             await self.bot.loop.run_in_executor(
                 self.executor,
@@ -35,6 +47,7 @@ class Runner(commands.Cog):
                     stdout=subprocess.PIPE,
                     stderr=subprocess.STDOUT,
                     shell=True,  # nosec
+                    env=env,
                 ),
             )
         ).stdout
