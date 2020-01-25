a556827a08297f9d90f666b41d8cd67be82f2a51
Michael H <michael@michaelhall.tech>
2020-01-25 13:35:43 -0500

    stricter matching of users

--- bansync/converters.py
+++ bansync/converters.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 from typing import NamedTuple, Optional, Set, Union
 
 import discord
-from redbot.core.commands import Context, BadArgument, MemberConverter
+from redbot.core.commands import Context, BadArgument
 from redbot.core.i18n import Translator
 
 _ = Translator("BanSync", __file__)
@@ -16,25 +16,19 @@ class ParserError(Exception):
     pass
 
 
-_discord_member_converter_instance = MemberConverter()
 _id_regex = re.compile(r"([0-9]{15,21})$")
 _mention_regex = re.compile(r"<@!?([0-9]{15,21})>$")
 
 
-class MemberOrID(NamedTuple):
-    member: Optional[discord.Member]
+class MentionOrID(NamedTuple):
     id: int
 
     @classmethod
     async def convert(cls, ctx: Context, argument: str):
 
-        with contextlib.suppress(Exception):
-            m = await _discord_member_converter_instance.convert(ctx, argument)
-            return cls(m, m.id)
-
         match = _id_regex.match(argument) or _mention_regex.match(argument)
         if match:
-            return cls(None, int(match.group(1)))
+            return cls(int(match.group(1)))
 
         raise BadArgument()
 
