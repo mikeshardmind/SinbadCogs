491ba498228c41cb7e21ca11287ff564547805fc
Michael H <michael@michaelhall.tech>
2020-01-30 11:18:30 -0500

    .

--- fortune/cows.py
+++ fortune/cows.py
@@ -3,14 +3,10 @@
 # cowsay <http://www.nog.net/~tony/warez/cowsay.shtml>
 # Licensed under the GNU LGPL version 3.0
 import textwrap
+from typing import List
 
 
-def cowsay(text, length=40):
-    return build_bubble(text, length) + build_cow()
-
-
-def build_cow():
-    return r"""
+COW_BASE = r"""
          \   ^__^
           \  (oo)\_______
              (__)\       )\/\
@@ -19,7 +15,11 @@ def build_cow():
     """
 
 
-def build_bubble(text, length=40):
+def cowsay(text, length=40) -> str:
+    return build_bubble(text, length) + COW_BASE
+
+
+def build_bubble(text: str, length=40) -> str:
     bubble = []
 
     lines = normalize_text(text, length)
@@ -38,7 +38,7 @@ def build_bubble(text, length=40):
     return "\n".join(bubble)
 
 
-def normalize_text(text, length):
+def normalize_text(text: str, length: int) -> List[str]:
     extra = ""
     attrib = "\n        \u2015"
     if attrib in text:
@@ -51,7 +51,7 @@ def normalize_text(text, length):
     return [line.ljust(maxlen) for line in lines]
 
 
-def get_border(lines, index):
+def get_border(lines: List[str], index: int) -> List[str]:
     if len(lines) < 2:
         return ["<", ">"]
 
