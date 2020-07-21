244af8012ae83a7500e4197a2d9458757fa387d3
Michael H <michael@michaelhall.tech>
2020-07-21 19:25:05 -0400

    [MLog] I didn't think this needed saying...

--- mlog/__init__.py
+++ mlog/__init__.py
@@ -1,7 +1,16 @@
+from redbot.core.errors import CogLoadError
 from .core import MLog
 
 
 def setup(bot):
+    if bot.user.id != 275047522026913793:
+        raise CogLoadError(
+            "Hey, stop that. "
+            "This was listed as WIP *and* hidden, "
+            "*and* warned you about *liabilities.* "
+            "Last warning, stop it. Stop trying to "
+            "log messages without understanding how badly this can go."
+        )
     cog = MLog(bot)
-    bot.add_cog()
+    bot.add_cog(cog)
     cog.init()
