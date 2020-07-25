36d807bb26a079ab3ddf5cc309c6190633e86c5c
Michael H <michael@michaelhall.tech>
2020-07-25 19:47:24 -0400

    Knowing how to spell doesn't prevent missing something through a lack of proper proofreading

--- mlog/core.py
+++ mlog/core.py
@@ -22,7 +22,7 @@ class MLog(commands.Cog):
     __red_end_user_data_statement__ = (
         "This cog logs messages and does not respect the data APIs. "
         "Bot owners have been warned against loading this cog as it is a work in progress. "
-        "Bot owners will recieve notice of attempts to delete data and it is on them to handle "
+        "Bot owners will receive notice of attempts to delete data and it is on them to handle "
         "this manually at the current time."
     )
 
