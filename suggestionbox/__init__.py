e3d3a72cca60b5b15e6633b29630b776253892e8
Michael H <michael@michaelhall.tech>
2020-08-07 15:33:33 -0400

    Resolves #248

--- suggestionbox/__init__.py
+++ suggestionbox/__init__.py
@@ -3,7 +3,8 @@ from .core import SuggestionBox
 __red_end_user_data_statement__ = (
     "This cog stores data provided to it by command as needed for operation. "
     "As this data is for suggestions to be given from a user to a community, "
-    "it is not reasonably considered end user data and will not be deleted."
+    "it is not reasonably considered end user data and will "
+    "not be deleted except as required by Discord."
 )
 
 
