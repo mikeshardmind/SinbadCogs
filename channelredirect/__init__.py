34516e49b96c13fb4019575cfbc4024f062b107e
Michael H <michael@michaelhall.tech>
2020-09-01 14:04:53 -0400

    A solution which can't be trivially bypassed
      - Solution can still be bypassed in theory, but it would be removing information
        about a license provided to end users in the process
    
      - The time spent on this commit was paid for by someone who after
        seeing how this was handled, stated an intention to stop using Red,
        but does not have another solution ready right now.
        They reached out and discussed a middle ground to avoid
        annoying users without allowing this to be easily bypassed
        like what I had recieved screenshots indicating people would
        do had I gone with a solution more similar to the prior time
        I informed of a lack of ongoing support.
    
      - Had someone from Red reached out rather than deciding to use this as
        a reason to announce the cogs were no longer approved using the same
        means as informing users of a security update (announcement channel
        publish & @everyone, something which had only been used for major
        issues since V3), I might have been more open to a simpler solution
        like #255 just being merged and taking the screenshots
        I had already recieved as people venting, not as real intent.

--- channelredirect/__init__.py
+++ channelredirect/__init__.py
@@ -1,3 +1,9 @@
+import asyncio
+from uuid import getnode
+
+from redbot.core import config
+from redbot.core.bot import Red
+
 from .redirect import ChannelRedirect
 
 __red_end_user_data_statement__ = (
@@ -6,14 +12,43 @@ __red_end_user_data_statement__ = (
 
 
 async def setup(bot):
-    await bot.send_to_owners(
-        "This cog still functions, but I suggest you leave and stop using Red. "
-        "I was removed from Red for not wanting my work misrepresented by the "
-        "organization, and stating what I would do *if* that continued. "
-        'For how much Red and it\'s members go after people who " take credit" '
-        "for their work, they sure were quick to dismiss mine. "
-        "The cog will recieve no further updates, nor is anyone legally allowed to fork to update."
-    )
+    asyncio.create_task(maybe_send_owners(bot))
     cog = ChannelRedirect(bot)
     bot.add_cog(cog)
     bot.before_invoke(cog.before_invoke_hook)
+
+
+async def maybe_send_owners(bot: Red):
+    # I have been provided multiple reasons to do it this way and not disable this completely.
+    # You can thank someone reaching out and having a discussion about this and a balance
+    # between not allowing issues being covered up or to be easily by passed up and not
+    # stranding users with no other options in the immediate term. (the reason I didn't just fundamentally break the cogs)
+    # This can still be circumvented, but that would be removing license information conveyed to end users.
+    await bot.wait_until_red_ready()
+    node_id = getnode()
+
+    conf = config.Config.get_conf(
+        None, identifier=node_id, cog_name=f"SinbadCogs-{node_id}"
+    )
+
+    conf.register_global(last_notify=[])
+
+    async with conf.last_notify.get_lock():
+        last_notify = await conf.last_notify()
+        cur_uptime = list(bot.uptime.timetuple()[:6])
+        if last_notify is None or cur_uptime > last_notify:
+
+            await bot.send_to_owners(
+                "This cog still functions, but I suggest you stop using Red and find an "
+                "alternative that cares about the contributions of volunteers or pays their devs. "
+                "I was removed from Red for not wanting my work misrepresented by the "
+                "organization, and stating what I would do *if* that continued. "
+                'For how much Red and it\'s members go after people who "take credit" '
+                "for their work, they sure were quick to dismiss mine. "
+                "The cog will likely recieve no further updates, nor does the license "
+                "(which can be found here: <https://github.com/mikeshardmind/SinbadCogs/blob/v3/LICENSE>) "
+                "permit public modifications by third parties."
+                "\nThis message was provided by a cog in <https://github.com/mikeshardmind/SinbadCogs/> "
+                "and an attempt will be made not to resend this message before the next bot restart."
+            )
+            await conf.last_notify.set(cur_uptime)
